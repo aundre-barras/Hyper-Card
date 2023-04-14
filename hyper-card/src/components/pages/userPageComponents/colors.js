f8b81dc7a5aa4796e38fdf80db5f68216375bfde
aundre-barras <barras@sonoma.edu>
2023-04-14 11:24:45 -0700

    added some base themes and a color pallete for testing

--- hyper-card/src/components/pages/userPageComponents/colors.js
+++ hyper-card/src/components/pages/userPageComponents/colors.js
@@ -1,6 +1,11 @@
-export const Colors = () => {
+export const Colors = (background_color, primary_color, text_color, secondary_text_color, button_color) => {
     return ([
-
-
+        {
+            background_color:"#e1dfb6",
+            primary_color:"#c5ee7d",
+            text_color:"#5c452d",
+            secondary_text_color:"#7a9244",
+            button_color:"#7a9244"
+        },
     ]);
 };
--- hyper-card/src/components/pages/userPageComponents/themes.js
+++ hyper-card/src/components/pages/userPageComponents/themes.js
@@ -1,6 +1,71 @@
-export const Themes = (main_color, secondary_color) => {
+export const Themes = (background_color, primary_color) => {
     return ([
-
-
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "backgroundImage":`linear-gradient(135deg, ${primary_color} 25%, transparent 25%), linear-gradient(225deg, ${primary_color} 25%, transparent 25%), linear-gradient(45deg, ${primary_color} 25%, transparent 25%), linear-gradient(315deg, ${primary_color} 25%, ${background_color} 25%)`,
+            "backgroundPosition":"18px 0, 18px 0, 0 0, 0 0",
+            "backgroundSize":"36px 36px",
+            "backgroundRepeat":"repeat",
+        },
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "background":`repeating-linear-gradient( 45deg, ${primary_color}, ${primary_color} 6px, ${background_color} 6px, ${background_color} 30px )`
+        },
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "background":`repeating-linear-gradient( -45deg, ${primary_color}, ${primary_color} 6px, ${background_color} 6px, ${background_color} 30px )`
+        },
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "backgroundImage":`linear-gradient(45deg, ${primary_color} 50%, ${background_color} 50%)`,
+            "backgroundSize":"22px 22px"
+        },
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "backgroundImage":`linear-gradient(-45deg, ${background_color}, ${background_color} 50%, ${primary_color} 50%, ${primary_color})`,
+            "backgroundSize":"22px 22px"
+        },
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "backgroundImage":`repeating-linear-gradient(45deg, ${primary_color} 25%, transparent 25%, transparent 75%, ${primary_color} 75%, ${primary_color}), repeating-linear-gradient(45deg, ${primary_color} 25%, ${background_color} 25%, ${background_color} 75%, ${primary_color} 75%, ${primary_color})`,
+            "backgroundPosition":"0 0, 22px 22px",
+            "backgroundSize":"44px 44px"
+        },
+        {   
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "background":`radial-gradient(circle at top left,transparent 25%,${primary_color} 25.5%, ${primary_color} 36%, transparent 37%, transparent 100%),radial-gradient(circle at bottom right,transparent 34%,${primary_color} 34.5%, ${primary_color} 45.5%, transparent 46%, transparent 100%)`,
+            "backgroundSize":"3em 3em"
+        },
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "background":`radial-gradient(circle at bottom left,transparent 25%,${primary_color} 25.5%, ${primary_color} 36%, transparent 37%, transparent 100%),radial-gradient(circle at top right,transparent 34%,${primary_color} 34.5%, ${primary_color} 45.5%, transparent 46%, transparent 100%)`,
+            "backgroundSize":"3em 3em"
+        },
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "background":`radial-gradient(circle, ${primary_color} 25%, transparent 26%),radial-gradient(circle at bottom left, ${primary_color} 12%, transparent 13%),radial-gradient(circle at bottom right, ${primary_color} 12%, transparent 13%),radial-gradient(circle at top left, ${primary_color} 12%, transparent 13%),radial-gradient(circle at top right, ${primary_color} 12%, transparent 13%)`,
+            "backgroundSize":"3em 3em"
+        },
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "background":`linear-gradient(-71deg, ${primary_color} 25%, transparent 26%, transparent 74%, ${primary_color} 75%)`,
+            "backgroundSize":"3em 3em"
+        },
+        {
+            "backgroundColor":`${background_color}`,
+            "opacity":"0.5",
+            "background":`linear-gradient(45deg,${primary_color} 20%, transparent 20%), linear-gradient(135deg, transparent 80%, ${primary_color} 80%), radial-gradient(${primary_color} 20% , transparent 21%)`,
+            "backgroundSize":"3em 3em"
+        }
     ]);
 };
